38edec1ad7e6501995c530aa93fd47c8a21de737
Maria <63523678+mariaachim@users.noreply.github.com>
2022-08-16 17:52:21 +0100

    can now find what day a date is yay

--- src/main/java/com/example/Calendar.java
+++ src/main/java/com/example/Calendar.java
@@ -16,13 +16,20 @@ import java.time.format.DateTimeFormatter;
 
 @Command(name = "calendar", mixinStandardHelpOptions = true, version = "calendar 1.0", description = "CLI events calendar")
 
-public class Calendar {
-    @Parameters(index = "0", description = "Date of event to add")
-    private LocalDate date;
-    public static void main(String... args)
-    {
+public class Calendar implements Runnable {
+    @Parameters(index = "0", paramLabel = "<date>", description = "Date of event to add")
+    private String dateStr;
+
+    @Override
+    public void run() {
+        DateTimeFormatter formatDate = DateTimeFormatter.ofPattern("d/M/yyyy");
+        LocalDate date = LocalDate.parse(dateStr, formatDate);
+        DayOfWeek day = DayOfWeek.from(date);
+        System.out.println(date + " is a " + day);
+    }
+    public static void main(String... args) {
         System.out.println( "Hello World!" );
-        int exitCode = new CommandLine(new Calendar()).execute(args));
+        int exitCode = new CommandLine(new Calendar()).execute(args);
         System.exit(exitCode);
     }
 }
